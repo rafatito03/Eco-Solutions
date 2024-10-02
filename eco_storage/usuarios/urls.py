56182e111703e94ace5d2f08f39b2b64bfe3a82e
biagalindoo <biagalindo4@gmail.com>
2024-10-02 09:42:32 -0300

    Add files via upload

---
+++ eco_storage/usuarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logout/', views.user_logout, name='logout'),
+    path('home/', views.home, name='home'),
+]
