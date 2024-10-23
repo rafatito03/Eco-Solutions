6b2ff06190f79c20d4cb707a488940d5c898d5ee
Rafael Peixoto <rmp2@cesar.school>
2024-10-22 21:43:59 -0300

    Update urls.py

--- eco_storage/urls.py
+++ eco_storage/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from usuarios import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('login/', views.user_login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('logout/', views.user_logout, name='logout'),
     path('home/', views.home, name='home'),
-    path('',views.mapa,name='mapa'),
+    path('mapa/',views.mapa,name='mapa'),
 ]
