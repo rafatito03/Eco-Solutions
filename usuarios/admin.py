d5183037d8440b403ee7a739ef86aa0258c26755
Rafael Peixoto <rmp2@cesar.school>
2024-10-22 21:42:15 -0300

    Update admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Residuos, Local, ONG
 
-# Register your models here.
+
+admin.site.register(ONG)
+admin.site.register(Residuos)
+admin.site.register(Local)
