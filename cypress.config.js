9b5b3780c36b0d5a8acf041dcc90084ae80577b0
Raphael Vasconcelos Vilela <167882931+rafatito03@users.noreply.github.com>
2024-11-27 17:56:25 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://127.0.0.1:8000",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    watchForFileChanges: false,
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
