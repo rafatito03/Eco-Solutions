4f7e41d88f4f990686d221a6a89abc391e68326a
Raphael Vilela <rvv@cesar.school>
2024-11-27 16:49:16 -0300

    candidato para versão final

--- usuarios/models.py
+++ usuarios/models.py
@@ -18,7 +18,7 @@ class ONG(models.Model):
     imagem = models.CharField(max_length=1000, null=True, blank=True, default='https://www.google.com/imgres?q=default%20coloque%20sua%20imagem&imgurl=https%3A%2F%2Fwww.quadrorama.com.br%2Fwp-content%2Fuploads%2F2018%2F05%2Fquadro-com-foto-imagem-personalizada.png&imgrefurl=https%3A%2F%2Fwww.quadrorama.com.br%2Fproduto%2Ffoto-personalizada%2F&docid=Rf3tWzXG5ZJ58M&tbnid=JyJzwRzOMJN5vM&vet=12ahUKEwiPsNa60_qJAxVYq5UCHfzfCAYQM3oECFAQAA..i&w=351&h=497&hcb=2&ved=2ahUKEwiPsNa60_qJAxVYq5UCHfzfCAYQM3oECFAQAA')
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='ong',null=True)
     telefone = models.CharField(max_length=30, default='(00) 00000-0000')
-    whatsapp = models.CharField(max_length=30, default='http://wa.me/5581994382896')
+    whatsapp = models.CharField(max_length=30, default='5581994382896')
     capacidade_maxima = models.FloatField(default=0)
     horario_funcionamento= models.CharField(max_length=255,default='das 8 ás 17h')
 
